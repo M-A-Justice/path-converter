3b7406412e0cd0a9f56bb7ba59d334e354f863b8
M-A-Justice <m_a_justice@yahoo.com>
2020-07-26 19:47:05 -0700

    complete windows to unix filepath function and minor change to ui basic styling

--- client/index.js
+++ client/index.js
@@ -1,10 +1,47 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable max-len */
 
-// Input:
-// C:\Users\m_a_j\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds\VoidTest\db\001469.ldb
-// Output:
-// c/Users/m_a_j/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/VoidTest/db/001469.ldb
+/* Input:
+  Windows filepaths
+
+  [C:]\Documents\Newsletters\Summer2018.pdf           An absolute file path from the root of drive C:
+  [A-Z]:
+
+  [\Program Files]\Custom Utilities\StringFinder.exe  An absolute path from the root of the current drive.
+  \\[\w\s-.]+
+
+  [2018]\January.xlsx                                A relative path to a file in a subdirectory of the current directory.
+  [\w\s-.]+
+
+  [..]\Publications\TravelBrochure.pdf              A relative path to file in a directory that is a peer of the current directory.
+  ..
+
+  [C:Projects]\apilibrary\apilibrary.sln            A relative path from the current directory of the C: drive.
+  [A-Z]:[\w\s-.]+
+
+  Regex intended to check for all of these cases
+  /^([A-Z]:|\\[\w\s-.]+|[\w\s-.]+|..|[A-Z]:[\w\s-.]+)?((\\[\w\s-.]+)+)*((\\)?[\w\s-.]+\.[a-zA-Z0-9]{1,4})$/gm
+
+  Linux filepaths
+
+  [c]/Users/m_a_j/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/VoidTest/db/001469.ldb
+  [a-z]
+
+  [/Users]/m_a_j/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/VoidTest/db/001469.ldb
+  [\/\w\s-.]+
+
+  [m_a_j]/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/VoidTest/db/001469.ldb
+  [\w\s-.]+
+
+  [/001469].ldb
+  [\/\w\s-.]+
+
+  001469.ldb
+
+  Output:
+    Windows -> Linux: c/Users/m_a_j/AppData/Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds/VoidTest/db/001469.ldb
+    Linux -> Windows: C:\Users\m_a_j\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds\VoidTest\db\001469.ldb
+*/
 
 /* ********* ONLY USED FOR TESTING ************
 const fs = require('fs');
@@ -18,14 +55,23 @@ const text = fs.readFileSync(path.join(__dirname, 'path.txt')).toString('utf-8')
 // filePath validator function
 const validPath = (filePath) => {
   // select file filePath selector for unix
-  const validUnix = /(^[a-zA-Z]\/)?([\w-.]+\/)+([\w-.]+\.[a-zA-Z0-9]{1,4})$/gm;
+  const validUnix = /(^[a-z])?((\/)?([\w\s-.]+))*(((\/)?[\w\s-.]+)\.[a-zA-Z0-9]{1,4})$/gm;
   // select file filePath selector for windows
-  const validWindows = /(^[A-Z]:\\)?([\w-.]+\\)+([\w-.]+\.[a-zA-Z0-9]{1,4})$/gm;
-  // if return for Unix regex match is the same length as the input filePath
-  // if return for Windows regex match is the same length as the input filePath
-  if (filePath.match(validUnix)[0].length === filePath.length || filePath.match(validWindows)[0].length === filePath.length) {
-    // return input filePath
-    return filePath;
+  const validWindows = /^([A-Z]:|\\[\w\s-.]+|[\w\s-.]+|..|[A-Z]:[\w\s-.]+)?((\\[\w\s-.]+)+)*((\\)?[\w\s-.]+\.[a-zA-Z0-9]{1,4})$/gm;
+
+  if (filePath.match(validWindows) !== null) {
+    // if return for Windows regex match is the same length as the input filePath
+    if (filePath.match(validWindows)[0].length === filePath.length) {
+      // return input filePath
+      return filePath;
+    }
+  }
+
+  if (filePath.match(validUnix) !== null) {
+    // if return for Unix regex match is the same length as the input filePath
+    if (filePath.match(validUnix)[0].length === filePath.length) {
+      return filePath;
+    }
   }
   // if the filePath doesnt match regex parameters return false
   return false;
@@ -33,11 +79,29 @@ const validPath = (filePath) => {
 
 // function to change to a unix filepath
 const updateUnixPath = (filePath) => {
-  // replace windows drive and slashes
-  filePath = filePath.replace(/([a-z])(:)/i, '$1').replace(/(\\+)/g, '/');
-  // make the first character lowercase
-  filePath = filePath.charAt(0).toLowerCase() + filePath.slice(1);
-  // return the completed filePath
+  const driveLetterColon = /^([a-z]:)/i;
+  const driveLetterNoColon = /^([a-z][^:])/i;
+  const directory = /^([^\\][\w\s-.]+)/;
+
+  if (directory.test(filePath)) {
+    filePath = filePath.replace(/(\\+)/g, '/');
+    return filePath;
+  }
+
+  if (driveLetterColon.test(filePath)) {
+    filePath = filePath.replace(/([a-z])(:)/i, '$1').replace(/(\\+)/g, '/');
+    filePath = filePath.charAt(0).toLowerCase() + filePath.slice(1);
+    filePath = '/mnt/'.concat(filePath);
+    return filePath;
+  }
+
+  if (driveLetterNoColon.test(filePath)) {
+    filePath = filePath.replace(/(\\+)/g, '/');
+    filePath = filePath.charAt(0).toLowerCase() + filePath.slice(1);
+    filePath = '/mnt/'.concat(filePath);
+    return filePath;
+  }
+  filePath = filePath.replace(/(\\+)/g, '/');
   return filePath;
 };
 
@@ -55,18 +119,25 @@ const updateWindowsPath = (filePath) => {
 const whichPath = (filePath) => {
   // const windows = /([a-z]:)/i;
   // const unix = /([a-z]\/)/i;
+  if (filePath === '') {
+    return 'Please enter a file path';
+  }
   // if filePath is not valid return invalidity
   if (validPath(filePath) === false) {
     return 'Not a valid path';
   }
-  // if filePath is not windows
+  // if filePath is Unix
   if (filePath.includes('/')) {
     // return windows filePath function result
     return updateWindowsPath(filePath);
     // otherwise
   }
-  // return unix filePath function result
-  return updateUnixPath(filePath);
+  // if filePath is Windows
+  if (filePath.includes('\\')) {
+    // return Unix filePath function result
+    return updateUnixPath(filePath);
+  }
+  return filePath;
 };
 
 module.exports = whichPath;
--- client/src/styles/App.style.js
+++ client/src/styles/App.style.js
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 100%;
   width: auto;
-  background-color: red;
   border: 2px solid black;
   padding: 30px;
 `;
